888776901feaaa12a4a5d348b448ac14469dd538
Jared Wasinger <j-wasinger@hotmail.com>
2022-12-27 22:00:22 -0500

    add tests

--- test.py
+++ test.py
@@ -21,25 +21,15 @@ def bench_geth(inp: str, code_file: str):
 
     geth_exec = os.path.join(geth_path)
     geth_cmd = "{} --codefile {} --input {} run".format(geth_exec, code_file, inp)
-    print(geth_cmd)
     result = subprocess.run(geth_cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if result.returncode != 0:
         raise Exception("geth exec error: {}".format(result.stderr))
 
     output = result.stdout.decode('utf-8')[2:-1]
-    import pdb; pdb.set_trace()
-    proj_point = (int(output[0:96], 16), int(output[96:192], 16), int(output[192:], 16))
-    z_inv = to_mont(fq_inv(to_norm(proj_point[2])))
-    affine_point = (to_norm(fq_mul(proj_point[0], z_inv)), to_norm(fq_mul(proj_point[1], z_inv)))
-    print("{}, {}".format(hex(affine_point[0]), hex(affine_point[1])))
+    return output
 
 def run_geth(inp):
-    bench_geth(inp, "build/artifacts/g1mul/g1mul_dbl_and_add.hex")
-
-def test1():
-    point = g1_gen()
-    scalar = 3
-    inp = encode_input(scalar, point)
+    return bench_geth(inp, "build/artifacts/g1mul/g1mul_dbl_and_add.hex")
 
 # test g1_gen * subgroup_order == inf_point
 def test_subgroup_order():
@@ -48,18 +38,32 @@ def test_subgroup_order():
     inp = encode_input(scalar, point)
     res = run_geth(inp)
 
-    assert res == '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+    assert res == '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+
+def test3():
+    point = g1_gen()
+    scalar = 3
+    inp = encode_input(scalar, point)
+    res = run_geth(inp)
+
+    assert res == '0a59e0a886919a3e3b746e549319d23dae362a728a29b6d804d02f86aeb43bd288207db6420a5d639d74409f5c53442d0eff673f0d01d28ace893c37229adaa3f139dcc81306308583c4fe2e7c5d70f9778e2d71e22804b2878f1b6d644f087b03adbc0d6fe485af1096a983741bf93f28cbbb920607abff8856ea833538197d094e67c9559d61baaaa51ff2f01bc682'
+
+def test1():
+    point = g1_gen()
+    scalar = 1
+    inp = encode_input(scalar, point)
+    output = run_geth(inp)
+
+    proj_point = (int(output[0:96], 16), int(output[96:192], 16), int(output[192:], 16))
+    z_inv = to_mont(fq_inv(to_norm(proj_point[2])))
+    affine_point = (to_norm(fq_mul(proj_point[0], z_inv)), to_norm(fq_mul(proj_point[1], z_inv)))
+
+    assert affine_point[0] == point.x and affine_point[1] == point.y
 
 def main():
-    # res = g1_gen()
-    # inp = encode_input(SUBGROUP_ORDER, g1_gen())
-
-    # inp = encode_input(3, g1_gen())
-    # bench_geth(inp, "build/artifacts/g1mul/g1mul_dbl_and_add.hex")
+    test1()
+    test3()
     test_subgroup_order()
-    import pdb; pdb.set_trace()
-    # TODO test g1mul(fq_order) == infinity point
-    # TODO test g1mul(2) == g1add(g1_gen, g1_gen)
 
 if __name__ == "__main__":
     main()
